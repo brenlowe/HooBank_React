668c49c1900c7247d712b9aa9f73a38da9d93516
Brennon Lowe <97804096+brenlowe@users.noreply.github.com>
2023-03-09 16:30:45 -0500

    first commit

---
+++ bank-app/src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import GetStarted from "./GetStarted";
+import Footer from "./Footer";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {  
+    Navbar,
+    Stats,
+    Business,
+    Billing,
+    CardDeal,
+    Testimonials,
+    Clients,
+    CTA,
+    Footer,
+    FeedbackCard,
+    Button,
+    GetStarted,
+    Hero
+}
